b5e8d6ae081f2510467946ea77e4c760a590ef0b
Peter DeWeese <peter.deweese@gmail.com>
2013-03-29 21:10:59 -0400

    Moved categories to ConventionalC.

--- terse-objective-c-ios/terse-objective-c-ios/Categories/UIBarButtonItem+terse_ios.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface UIBarButtonItem (terse_ios)
-
-+(UIBarButtonItem*)barButtonItemWithCustomView:(UIView*)v;
-+(UIBarButtonItem*)barButtonItemWithTitle:(NSString*)title style:(UIBarButtonItemStyle)style target:(id)target action:(SEL)action;
-+(UIBarButtonItem*)barButtonItemWithBarButtonSystemItem:(UIBarButtonSystemItem)item target:(id)target action:(SEL)action;
-
-@end
--- terse-objective-c-ios/terse-objective-c-ios/terse_objective_c_ios.h
+++ terse-objective-c-ios/terse-objective-c-ios/terse_objective_c_ios.h
@@ -1,11 +1,4 @@
 #import <Foundation/Foundation.h>
-#import "NSArray+terse_ios.h"
-#import "NSBundle+terse_ios.h"
-#import "NSDate+terse_ios.h"
-#import "UIBarButtonItem+terse_ios.h"
-#import "UIDevice+terse_ios.h"
-#import "UIToolbar+terse_ios.h"
-#import "UIView+terse_ios.h"
 
 #define $format(format, objs...) [NSString stringWithFormat: format, objs]
 
